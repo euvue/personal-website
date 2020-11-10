351439c0bff4ecaa7bd48e42e51390680fae6aca
euvue <eeuv@tuta.io>
2020-11-09 22:23:45 -0700

    modified App.vue & HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <HelloWorld msg="Welcome to Hmong Free Open Source Collective" />
 </template>
 
 <script>
